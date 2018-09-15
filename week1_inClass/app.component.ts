01e25219a881ddc035810efe48010f151bd4c675
Christina Boyd <chris121978@gmail.com>
2018-09-15 14:44:00 -0700

    exercise 5

--- week1_inClass/app.component.ts
+++ week1_inClass/app.component.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
-})
-export class AppComponent {
-  title = 'myapp';
+export class PlayingCard {
+    cardVal: string;
+    suit: string;
 }
+
+@Component({
+    selector: 'app-root',
+    // Multi-line content allowed with back ticks.
+    template: `<h1>Hello world!  {{title}} <br/>
+              <!-- Show cards in unordered list. -->
+              <ul><li *ngFor="let card of cards" (click)="onSelect(card)">
+                  {{card.cardVal}}</li>
+             </ul>
+             <div *ngIf="selectedCard">
+                 <h2>{{selectedCard.cardVal}} **</h2>
+                 <input [(ngModel)]="selectedCard.cardVal" placeholder="name"/>
+              </div>
+			  `
+})
+
+export class AppComponent { 
+    public title = 'This is Angular!';
+    // Include card data in class as public property.
+    public cards = CARDS; 
+    selectedCard: PlayingCard;
+	
+    onSelect(card: PlayingCard) { 
+        this.selectedCard = card;
+        alert(card.suit); 
+    }
+}
+
+// Define card data.
+var CARDS: PlayingCard[] = [
+    { cardVal:"Ace", suit:"Spades"},
+    { cardVal:"Two", suit:"Clubs" },
+    { cardVal:"Six", suit:"Hearts"},
+];
+
